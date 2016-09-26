04c5c179e16c97b0713885b49bf5cc2c054e0b36
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2016-09-26 21:27:55 +0300

    feat: 6 extra couchdb endpoints
    * Database
    
    - insert
    - delete
    - get
    - head
    - bulk
    - list

--- time2relax/time2relax.py
+++ time2relax/time2relax.py
@@ -15,7 +15,7 @@ class Server(object):
     def __init__(self, url=COUCHDB_URL):
         """Initialize the server object."""
 
-        self._c = HTTPClient()
+        self.client = HTTPClient()
         self.url = url
 
     def __repr__(self):
@@ -25,18 +25,21 @@ class Server(object):
     def auth(self, username, password):
         """Authenticates user by Cookie-based user login."""
 
-        url = urljoin(self.url, '_session')
-        data = {'name': username, 'password': password}
+        data = {
+            'name': username,
+            'password': password,
+        }
 
-        return self.request('POST', url, json=data)
+        return self.request('POST', '_session', json=data)
 
     # http://docs.couchdb.org/en/latest/api/database/compact.html#post--db-_compact
     def compact(self, name, ddoc=None):
         """Starts a compaction for the database or ddoc."""
 
-        url = '/'.join([urljoin(self.url, name), '_compact'])
+        url = os.path.join(encode_url_database(name), '_compact')
+
         if ddoc:
-            url = '/'.join([url, ddoc])
+            url = os.path.join(url, ddoc)
 
         return self.request('POST', url, json={})
 
@@ -44,56 +47,54 @@ class Server(object):
     def create(self, name):
         """Creates a new database."""
 
-        url = urljoin(self.url, name)
-
-        return self.request('PUT', url)
+        return self.request('PUT', encode_url_database(name))
 
     # http://docs.couchdb.org/en/latest/api/database/common.html#delete--db
     def delete(self, name):
         """Deletes an existing database."""
 
-        url = urljoin(self.url, name)
-
-        return self.request('DELETE', url)
+        return self.request('DELETE', encode_url_database(name))
 
     # http://docs.couchdb.org/en/latest/api/database/common.html#get--db
     def get(self, name):
         """Returns the database information."""
 
-        url = urljoin(self.url, name)
-
-        return self.request('GET', url)
+        return self.request('GET', encode_url_database(name))
 
     # http://docs.couchdb.org/en/latest/api/server/common.html#get--_all_dbs
     def list(self):
         """Returns a list of all the databases."""
 
-        url = urljoin(self.url, '_all_dbs')
-
-        return self.request('GET', url)
+        return self.request('GET', '_all_dbs')
 
     # http://docs.couchdb.org/en/latest/api/server/common.html#replicate
     def replicate(self, name, target, options=None):
         """Starts or cancels the replication."""
 
-        url = urljoin(self.url, '_replicate')
-        data = {'source': name, 'target': target}
+        data = {
+            'source': name,
+            'target': target,
+        }
+
         if options:
             data.update(options)
 
-        return self.request('POST', url, json=data)
+        return self.request('POST', '_replicate', json=data)
 
     # http://docs.couchdb.org/en/latest/api/server/authn.html#get--_session
     def session(self):
         """Returns Cookie-based login user information."""
 
-        url = urljoin(self.url, '_session')
-
-        return self.request('GET', url)
+        return self.request('GET', '_session')
 
     def request(self, method, url, **kwargs):
         """Constructs and sends a request."""
 
+        u = urljoin(self.url, url)
+        k = kwargs
+
+        if 'params' in kwargs and kwargs['params']:
+            k['params'] = format_url_params(kwargs['params'])
 
         # Pipe to the HTTP client
         return self.client.request(method, u, **k)
@@ -105,19 +106,93 @@ class Database(object):
     def __init__(self, server, name):
         """Initialize the database object."""
 
-        self._s = server
         self.name = name
+        self.server = server
         self.url = urljoin(server.url, name)
 
     def __repr__(self):
-        return '<{0} [{1}]>'.format(self.__class__.__name__, self.url)
+        return '<{0} [{1}]>'.format(self.__class__.__name__, self.name)
 
-    def request(self, method, url, **kwargs):
+    # http://docs.couchdb.org/en/latest/api/document/common.html#put--db-docid
+    # http://docs.couchdb.org/en/latest/api/database/common.html#post--db
+    def insert(self, doc, params=None):
+        """Creates or updates a document."""
+
+        if '_id' in doc:
+            method = 'PUT'
+            url = doc['_id']
+        else:
+            method = 'POST'
+            url = None
+
+        return self.request(method, url, params=params, json=doc)
+
+    # http://docs.couchdb.org/en/latest/api/document/common.html#delete--db-docid
+    def delete(self, _id, rev):
+        """Deletes the document."""
+
+        return self.request('DELETE', _id, params={'rev': rev})
+
+    # http://docs.couchdb.org/en/latest/api/document/common.html#get--db-docid
+    def get(self, _id, params=None):
+        """Returns the document."""
+
+        return self.request('GET', _id, params=params)
+
+    # http://docs.couchdb.org/en/latest/api/document/common.html#head--db-docid
+    def head(self, _id):
+        """Returns bare information in the HTTP Headers for the document."""
+
+        return self.request('HEAD', _id)
+
+    # http://docs.couchdb.org/en/latest/api/database/bulk-api.html#post--db-_bulk_docs
+    def bulk(self, docs, options=None):
+        """Inserts or updates multiple documents in to the database in a single
+        request."""
+
+        data = {'docs': docs}
+
+        if options:
+            data.update(options)
+
+        return self.request('POST', '_bulk_docs', json=data)
+
+    # http://docs.couchdb.org/en/latest/api/database/bulk-api.html#get--db-_all_docs
+    def list(self, params=None):
+        """Returns a built-in view of all documents in this database."""
+
+        return self.request('GET', '_all_docs', params=params)
+
+    def request(self, method, url=None, **kwargs):
         """Constructs and sends a request."""
 
-        return self._s.request(method, url, **kwargs)
+        u = encode_url_database(self.name)
+
+        if url:
+            u = os.path.join(u, url)
+
+        return self.server.request(method, u, **kwargs)
 
 
+# http://wiki.apache.org/couchdb/HTTP_database_API#Naming_and_Addressing
+def encode_url_database(url):
+    """Encodes a database url."""
+
+    return quote(url, "~()*!.\'")
 
 
+def format_url_params(params):
+    """Formats set url params."""
 
+    p = {}
+
+    for k, v in params.items():
+        if type(v) is str:
+            v = '"{0}"'.format(v)
+        elif v is True:
+            v = 'true'
+        elif v is False:
+            v = 'false'
+        p[k] = v
+
+    return p
