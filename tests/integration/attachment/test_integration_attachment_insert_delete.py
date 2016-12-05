03da39c7c4bd048d196d30e4c2795340abaf0823
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2016-12-05 23:24:25 +0300

    test: merge attachment insert & delete

--- tests/integration/attachment/test_integration_attachment_insert_delete.py
+++ tests/integration/attachment/test_integration_attachment_insert_delete.py
@@ -2,7 +2,7 @@
 
 from time2relax import Database
 
-FIXTURE = ['attachment', 'insert']
+FIXTURE = ['attachment', 'insert_delete']
 
 
 def test_insert(server, db_name):
@@ -14,5 +14,17 @@ def test_insert(server, db_name):
     # Update document '_rev'
     doc = db.get('foobaz').json()
     j = db.att_insert(doc, 'att', 'Hello World!', 'text/plain').json()
+
+    assert j['ok'] is True
+
+
+def test_delete(server, db_name):
+    """Should delete an attachment."""
+
+    db = Database(server, db_name)
+
+    # Update document '_rev'
+    doc = db.get('foobaz').json()
+    j = db.att_delete(doc, 'att').json()
 
     assert j['ok'] is True
