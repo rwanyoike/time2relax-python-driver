0422c2a18ae6b217f30d5f20346e646cb1d8c07e
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2016-12-30 23:18:51 +0300

    fix: bypass test instance crash
    See: 713871f677068fbdba0eba6eb410d251140c014f

--- tests/integration/test_compact.py
+++ tests/integration/test_compact.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import time
+
 
 def test_compact(db):
     db.insert({'_id': 'someid'})
     response = db.compact()
+    time.sleep(2)
     result = response.json()
 
     assert 'ok' in result
@@ -11,5 +14,6 @@ def test_compact(db):
 
 def test_compact_kwargs(db):
     response = db.compact(json={}, headers={'X-Assert': 'true'})
+    time.sleep(2)
 
     assert 'X-Assert' in response.request.headers
