ff32af2e0f06c08cf5d4a44222f5af69bdf2718f
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-01-03 12:39:14 +0300

    test: organize files

--- tests/integration/test_integration_request.py
+++ tests/integration/test_integration_request.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-from responses import RequestsMock
-
-import time2relax
-from time2relax import CouchDB
-
 
 def test_request(db):
     params = {'a': True, 'b': False, 'c': 'cat'}
@@ -14,30 +8,3 @@ def test_request(db):
     assert 'a=true' in r.request.url
     assert 'b=false' in r.request.url
     assert 'c=cat' in r.request.url
-
-
-def test_request_exceptions(db):
-    db = CouchDB(db.url, True)
-
-    exceptions = {
-        400: time2relax.BadRequest,
-        401: time2relax.Unauthorized,
-        403: time2relax.Forbidden,
-        404: time2relax.ResourceNotFound,
-        405: time2relax.MethodNotAllowed,
-        409: time2relax.ResourceConflict,
-        412: time2relax.PreconditionFailed,
-        500: time2relax.ServerError,
-        999: time2relax.CouchDBError,
-    }
-
-    rm = RequestsMock()
-    rm.start()
-
-    for i in exceptions:
-        rm.add('GET', db.url, status=i)
-        with pytest.raises(exceptions[i]):
-            db._request('GET', db.url)
-
-    rm.stop()
-    rm.reset()
---
+++ tests/unit/test_unit_request.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from responses import RequestsMock
+
+import time2relax
+from time2relax import CouchDB
+
+
+def test_request(db):
+    db = CouchDB(db.url, True)
+
+    exceptions = {
+        400: time2relax.BadRequest,
+        401: time2relax.Unauthorized,
+        403: time2relax.Forbidden,
+        404: time2relax.ResourceNotFound,
+        405: time2relax.MethodNotAllowed,
+        409: time2relax.ResourceConflict,
+        412: time2relax.PreconditionFailed,
+        500: time2relax.ServerError,
+        999: time2relax.CouchDBError,
+    }
+
+    rm = RequestsMock()
+    rm.start()
+
+    for i in exceptions:
+        rm.add('GET', db.url, status=i)
+        with pytest.raises(exceptions[i]):
+            db._request('GET', db.url)
+
+    rm.stop()
+    rm.reset()
