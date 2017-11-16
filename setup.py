d1252f402861d46eb96c24e137c4d81f8e9c61a3
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-11-16 22:02:49 +0300

    feat: add pypy test support

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     test_suite='tests',
     tests_require=test_requirements,
