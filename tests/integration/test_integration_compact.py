d4de2abbfd8e0bfd0b61b0d38eabae134a1f0cc7
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2017-11-27 14:18:22 +0300

    test: increase compact wait time
    - Test fails 1/2 of the time.

--- tests/integration/test_integration_compact.py
+++ tests/integration/test_integration_compact.py
@@ -6,11 +6,11 @@ import time
 def test_compact(db):
     db.insert({'_id': 'someid'})
     r = db.compact()
-    time.sleep(2)  # wait for compact
+    time.sleep(5)  # wait for compact
     assert 'ok' in r.json()
 
 
 def test_compact_kwargs(db):
     r = db.compact(json={}, headers={'X-Assert': 'true'})
-    time.sleep(2)  # wait for compact
+    time.sleep(5)  # wait for compact
     assert 'X-Assert' in r.request.headers
