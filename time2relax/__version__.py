10b704ce634d45effe427f9eda78cbf634f8303f
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2018-08-04 23:21:52 +0300

    Bump version: 0.4.1 → 0.5.2

--- time2relax/__version__.py
+++ time2relax/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'time2relax'
 __description__ = 'A CouchDB driver for Python.'
 __url__ = 'https://github.com/rwanyoike/time2relax'
-__version__ = '0.4.1'
+__version__ = '0.5.2'
 __author__ = 'Raymond Wanyoike'
 __author_email__ = 'raymond.wanyoike@gmail.com'
 __license__ = 'MIT'
